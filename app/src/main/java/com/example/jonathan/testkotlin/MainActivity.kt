4ead6bce77a5a8f10d6c09f8bfa28da3dd3c0e51
Jonathan Zhong <33461789+jonathanzho@users.noreply.github.com>
2019-02-27 01:09:37 -0800

    Tried Log.d()

--- app/src/main/java/com/example/jonathan/testkotlin/MainActivity.kt
+++ app/src/main/java/com/example/jonathan/testkotlin/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.jonathan.testkotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("TKT MainActivity", "onCreate")
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
